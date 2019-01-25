9fc2eaa4084027dced6b834d20499b6bbef80f18
ej <ej@ejs-MacBook-Pro.local>
2019-01-24 20:24:00 -0500

    Step 2 - Add a Shadow Dom Element

--- icon-toggle.js
+++ icon-toggle.js
@@ -2,23 +2,30 @@ import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 import '@polymer/iron-icon/iron-icon.js';
 
 class IconToggle extends PolymerElement {
+
   static get template() {
+
     return html`
       <style>
         /* shadow DOM styles go here */
-        span {
-          color: blue;
-        }
         :host {
           display: inline-block;
         }
+        iron-icon {
+          fill: rgba(0,0,0,0);
+          stroke: currentcolor;
+        }
+        :host([pressed]) iron-icon {
+          fill: currentcolor;
+        }
       </style>
-  
       <!-- shadow DOM goes here -->
-      <span>Not much here yet.</span>
+      <iron-icon icon="polymer"></iron-icon>
     `;
   }
+
   constructor() {
+    //ALWAYS FIRST LINE OF CONSTRUCTOR
     super();
   }
 }
